a63f3aa323c60e3842a987f7616dc1c928e2c5ca
Adam Sobotka <vorcigernix@gmail.com>
2022-10-15 09:43:41 +0200

    just trying everything

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,14 @@ import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: true
+		})
 	},
 	preprocess: [
 		preprocess({
