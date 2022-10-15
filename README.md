7687a594ba480cd5455153be49b50464d6a9d20b
tree <tree@hidden.cz>
2022-10-15 16:42:41 +0200

    Move data build to script

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # ETHBrno² Website
 
 Website is created using [SvelteKit](https://kit.svelte.dev/) and deployed as a static application to these addresses:
-* (ethbrno.cz)[https://ethbrno.cz]
+* [ethbrno.cz](https://ethbrno.cz) (self-hosted)
+* [ethbrno.netlify.app](https://ethbrno.netlify.app/) ([Netlify](https://www.netlify.com/) mirror)
 
 ## Authors
 
---
+++ scripts/make-data.js
@@ -0,0 +1,19 @@
+import yaml from 'js-yaml'
+import fs from 'fs'
+
+const DATA_FILE = './data/data.yaml'
+const DATA_OUTPUT = './src/lib/data.json'
+
+function gen() {
+  // load data
+  const data = yaml.load(fs.readFileSync(DATA_FILE))
+
+  // add generation time
+  data.time = new Date
+
+  // write to json
+  fs.writeFileSync(DATA_OUTPUT, JSON.stringify(data, null, 2))
+  console.log(`Writed: ${DATA_OUTPUT}`)
+}
+
+gen()
