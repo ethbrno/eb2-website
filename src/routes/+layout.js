1612a25449081d2d98b9844492466112d584975f
Adam Sobotka <vorcigernix@gmail.com>
2022-10-15 12:29:46 +0200

    move json and import it as a local resource

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,10 +1,13 @@
 /** @type {import('./$types').PageLoad} */
 export const prerender = true;
+//import { contributors, sponsors } from "$lib/data.json";
+import { contributors } from "$lib/data.json";
 
 export async function load({ fetch }) {
     //const { contributors, sponsors } = await fetch(`data.json`).then((r) => r.json());
-    const { contributors, sponsors } = await fetch(`https://raw.githubusercontent.com/ethbrno/data/main/data.json`).then((r) => r.json());
+    //const { contributors, sponsors } = await fetch(`https://raw.githubusercontent.com/ethbrno/data/main/data.json`).then((r) => r.json());
     return {
-        data: { contributors, sponsors }
+        //data: { contributors, sponsors }
+        data: { contributors }
     };
 }
