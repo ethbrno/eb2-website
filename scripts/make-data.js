3f0633aa81ee43d5941d85840a80270fa6ba9409
tree <tree@hidden.cz>
2022-10-20 15:14:21 +0200

    Versioning

--- scripts/make-data.js
+++ scripts/make-data.js
@@ -3,13 +3,22 @@ import fs from 'fs'
 
 const DATA_FILE = './data/data.yaml'
 const DATA_OUTPUT = './src/lib/data.json'
+const PKG_FILE = './package.json'
+const VERSION_PATTERN = /^v\d{4}-\d{2}-\d{2}$/
 
 function gen() {
   // load data
   const data = yaml.load(fs.readFileSync(DATA_FILE))
+  const pkg = JSON.parse(fs.readFileSync(PKG_FILE))
+
+  if (!pkg.version.match(VERSION_PATTERN)) {
+    console.error(`Version not match pattern [${VERSION_PATTERN}]: ${pkg.version}`)
+    process.exit(1)
+  }
 
   // add generation time
   data.time = new Date
+  data.version = pkg.version
 
   // write to json
   fs.writeFileSync(DATA_OUTPUT, JSON.stringify(data, null, 2))
