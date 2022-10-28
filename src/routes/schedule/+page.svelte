30added2c008be71a7af7eed0f845353ab644a05
Adam Sobotka <vorcigernix@gmail.com>
2022-10-28 17:06:54 +0200

    schema update

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -2,12 +2,11 @@
 	export let data;
 	//const selected = new Date().getDay();
 	let selectedDay = 6;
-	$: filteredEvents = data.events.filter((e) => {
-		console.log(new Date(e.datetime).getDay(), selectedDay);
-		//new Date(e.datetime).getDay() === selected;
-		true;
+	let filteredEvents = data.events.filter((c) => {
+		let evtDate = new Date(c.datetime).getDay();
+		return evtDate === selectedDay;
 	});
-	//console.log(filteredEvents);
+	console.log(filteredEvents);
 </script>
 
 <section
@@ -37,36 +36,32 @@
 				<div class="py-8 flex flex-wrap md:flex-nowrap">
 					<div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
 						<span class="font-semibold title-font text-white">3pm-4pm</span>
-						<a href="/venues" class="mt-1 text-white text-sm mb-1 hover:underline">🞋 Atrium</a>
-						<span class="mt-1 text-gray-500 text-sm">WORKSHOP</span>
+						<a href="/venues" class="mt-1 text-white text-sm mb-1 hover:underline">🞋 {evt.stage}</a>
+						<span class="mt-1 text-gray-500 text-sm uppercase">{evt.eventtype}</span>
 					</div>
 					<div class="md:flex-grow">
 						<h2 class="text-2xl font-medium text-white title-font mb-2">
-							How to build censorship resistant web
+							{evt.name}
 						</h2>
 						<p class="leading-relaxed">
-							This web is super hard to take down. We doubt that any government would try, despite
-							our growing popularity. But even if they do, they will fail. Learn why, what option
-							you have and build a similar web with Adam.
+							{evt.description}
 						</p>
 						<div class="col-start-2 row-start-1 row-end-3 mt-4 lg:mt-0 xl:mt-4">
-							<dt class="sr-only">Users</dt>
+							<dt class="sr-only">Speakers</dt>
 							<dd class="flex justify-start -space-x-1.5">
-								<img
-									src="/photos/contributors/adam.jpg"
-									alt="Adam"
-									class="w-6 h-6 rounded-full bg-slate-100 ring-2 ring-white grayscale"
-									loading="lazy"
-								/>
-								<img
-									src="/photos/contributors/anett.jpg"
-									alt="Adam"
-									class="w-6 h-6 rounded-full bg-slate-100 ring-2 ring-white grayscale"
-									loading="lazy"
-								/>
-								<p class="pl-6">
-									<a href="/contributors">Adam Sobotka</a> +
-									<a href="/contributors">Anett Rolikova</a>
+								{#each evt.speakers as speaker}
+									<img
+										src={`/photos/contributors/${speaker.id}.jpg`}
+										alt={speaker.name}
+										class="w-6 h-6 rounded-full bg-slate-100 ring-2 ring-white grayscale"
+										loading="lazy"
+									/>
+								{/each}
+
+								<p class="pl-4 flex space-x-2 divide-x ">
+									{#each evt.speakers as speaker}
+										<a class="pl-2" href={speaker.id}>{speaker.name}</a>
+									{/each}
 								</p>
 							</dd>
 						</div>
