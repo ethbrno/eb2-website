ed0f0370d16ce53d603a1b1754ab2faafb4d61ba
Adam Sobotka <vorcigernix@gmail.com>
2022-10-28 17:46:01 +0200

    v1

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,8 +1,9 @@
 <script>
 	export let data;
-	//const selected = new Date().getDay();
-	let selectedDay = 6;
-	let filteredEvents = data.events.filter((c) => {
+	const today = new Date().getDay();
+	let selectedDay = today === 5 || today === 6 || today === 0 ? today : 6;
+	//let selectedDay = 6;
+	$: filteredEvents = data.events.filter((c) => {
 		let evtDate = new Date(c.datetime).getDay();
 		return evtDate === selectedDay;
 	});
@@ -13,17 +14,20 @@
 	class="text-white body-font bg-black px-2 py-6 md:py-12 flex justify-center flex-row overflow-x-scroll flex-wrap text-sm md:text-base"
 >
 	<button
-		class="px-3 py-2 md:px-5 md:py-2 border m-2 {true
+		on:click={() => (selectedDay = 5)}
+		class="px-3 py-2 md:px-5 md:py-2 border m-2 {selectedDay === 5
 			? 'text-black bg-white'
 			: 'hover:outline-none hover:bg-gray-600'}">Friday</button
 	>
 	<button
-		class="px-3 py-2 md:px-5 md:py-2 border m-2 {false
+		on:click={() => (selectedDay = 6)}
+		class="px-3 py-2 md:px-5 md:py-2 border m-2 {selectedDay === 6
 			? 'text-black bg-white'
 			: 'hover:outline-none hover:bg-gray-600'}">Saturday</button
 	>
 	<button
-		class="px-3 py-2 md:px-5 md:py-2 border m-2 {false
+		on:click={() => (selectedDay = 0)}
+		class="px-3 py-2 md:px-5 md:py-2 border m-2 {selectedDay === 0
 			? 'text-black bg-white'
 			: 'hover:outline-none hover:bg-gray-600'}">Sunday</button
 	>
@@ -34,11 +38,20 @@
 		{#each filteredEvents as evt}
 			<div class="-my-8 divide-y-2 divide-gray-800">
 				<div class="py-8 flex flex-wrap md:flex-nowrap">
-					<div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-						<span class="font-semibold title-font text-white">3pm-4pm</span>
-						<a href="/venues" class="mt-1 text-white text-sm mb-1 hover:underline">🞋 {evt.stage}</a>
-						<span class="mt-1 text-gray-500 text-sm uppercase">{evt.eventtype}</span>
-					</div>
+					{#if evt.eventtype === 'Key'}
+						<div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+							<div><span class="font-bold title-font text-black bg-white p-5">3pm-4pm</span></div>
+						</div>
+					{:else}
+						<div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+							<span class="font-semibold title-font text-white">3pm-4pm</span>
+							<a href="/venues" class="mt-1 text-white text-sm mb-1 hover:underline"
+								>🞋 {evt.stage}</a
+							>
+							<span class="mt-1 text-gray-500 text-sm uppercase">{evt.eventtype}</span>
+						</div>
+					{/if}
+
 					<div class="md:flex-grow">
 						<h2 class="text-2xl font-medium text-white title-font mb-2">
 							{evt.name}
@@ -49,7 +62,7 @@
 						<div class="col-start-2 row-start-1 row-end-3 mt-4 lg:mt-0 xl:mt-4">
 							<dt class="sr-only">Speakers</dt>
 							<dd class="flex justify-start -space-x-1.5">
-								{#each evt.speakers as speaker}
+								{#each evt.speakers || [] as speaker}
 									<img
 										src={`/photos/contributors/${speaker.id}.jpg`}
 										alt={speaker.name}
@@ -59,7 +72,7 @@
 								{/each}
 
 								<p class="pl-4 flex space-x-2 divide-x ">
-									{#each evt.speakers as speaker}
+									{#each evt.speakers || [] as speaker}
 										<a class="pl-2" href={speaker.id}>{speaker.name}</a>
 									{/each}
 								</p>
