3f48b3c9fee49a832bc9edb423ada8684f86c550
tree <tree@hidden.cz>
2022-10-31 06:46:15 +0100

    Force hacker manual to reload

--- src/routes/manual/+page.js
+++ src/routes/manual/+page.js
@@ -4,7 +4,7 @@ import { marked } from 'marked';
 
 marked.setOptions({ gfm: true });
 export async function load({ fetch }) {
-    const response = await fetch(`${base}/hacker-manual.md`).then((r) => r.text());
+    const response = await fetch(`${base}/hacker-manual.md?${Date.now()}`).then((r) => r.text());
 
     const compiledResponse = await marked.parse(response);
     return { content: compiledResponse };
